5c3c60dd14fbc96043c9411d45c2f68744566b4f
vrg8778 <victordias8778@gmail.com>
2024-09-28 18:07:48 -0300

    subindo arquivo

---
+++ tarefas.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+
+def criar_janela_inicial():
+    sg.theme('Black')
+    layout_tarefa = [
+        [sg.Checkbox(''), sg.Input('', border_width=0)]
+    ]
+    layout = [
+        [sg.Frame("Tarefas", layout=layout_tarefa, key='container')],
+        [sg.Button("Nova tarefa"),sg.Button("Resetar")]
+    ]
+
+    return sg.Window("Tarefas", layout=layout, finalize=True)
+
+janela = criar_janela_inicial()
+
+while True:
+    event,value = janela.read()
+    if event == sg.WIN_CLOSED:
+        break
+    if event == "Nova tarefa":
+        janela.extend_layout(janela["container"], [[sg.Checkbox(''), sg.Input('')]])
+    if event == "Resetar":
+        janela.close()
+        janela = criar_janela_inicial()
